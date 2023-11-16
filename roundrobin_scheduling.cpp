15aeec1c8672e9ae21f2ebe0d3c99a102c86024f
Nathan Gonçalves <nathan@liberaai.com>
2023-11-16 15:37:44 -0300

    Algoritmo de Escalonamento Round Robin

--- roundrobin_scheduling.cpp
+++ roundrobin_scheduling.cpp
@@ -2,6 +2,12 @@
 #include <algorithm> 
 #include <iomanip>
 #include <queue> 
+#include <random>
+#include <cstring>
+
+#define NUM_PROCESSOS 10;
+#define TEMPO_POR_PROCESSO 1;
+
 using namespace std;
 
 struct process {
@@ -15,21 +21,34 @@ struct process {
     int response_time;
 };
 
-bool compare1(process p1, process p2) 
-{ 
-    return p1.arrival_time < p2.arrival_time;
-}
+// bool compare1(process p1, process p2) 
+// { 
+//     return p1.arrival_time < p2.arrival_time;
+// }
 
-bool compare2(process p1, process p2) 
+bool organizarPorPids(process p1, process p2) 
 {  
     return p1.pid < p2.pid;
 }
 
-int main() {
+void geraProcessos(queue<int> *q, int nProcessos, int burst_cpy[], struct process p[]){
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<> dis(0, 15);
+    for(int i = 0; i < nProcessos; i++) {
+        p[i].pid = i+1;
+        p[i].arrival_time = dis(gen);
+        p[i].burst_time = dis(gen);
+        burst_cpy[i] = p[i].burst_time;
 
-    int n;
-    int tq;
-    struct process p[100];
+        q->push(i);
+    }
+}
+
+int main() {
+    int nProcessos = NUM_PROCESSOS;
+    int tempoQuantum = TEMPO_POR_PROCESSO;
+    struct process p[nProcessos];
     float avg_turnaround_time;
     float avg_waiting_time;
     float avg_response_time;
@@ -39,53 +58,35 @@ int main() {
     int total_response_time = 0;
     int total_idle_time = 0;
     float throughput;
-    int burst_remaining[100];
+    int burst_cpy[nProcessos];
     int idx;
+
+    queue<int> q;
 
     cout << setprecision(2) << fixed;
 
-    cout<<"Enter the number of processes: ";
-    cin>>n;
-    cout<<"Enter time quantum: ";
-    cin>>tq;
+    geraProcessos(&q, nProcessos, burst_cpy, p);
 
-    for(int i = 0; i < n; i++) {
-        cout<<"Enter arrival time of process "<<i+1<<": ";
-        cin>>p[i].arrival_time;
-        cout<<"Enter burst time of process "<<i+1<<": ";
-        cin>>p[i].burst_time;
-        burst_remaining[i] = p[i].burst_time;
-        p[i].pid = i+1;
-        cout<<endl;
-    }
-
-    sort(p,p+n,compare1);
-
-    queue<int> q;
     int current_time = 0;
-    q.push(0);
     int completed = 0;
-    int mark[100];
-    memset(mark,0,sizeof(mark));
-    mark[0] = 1;
 
-    while(completed != n) {
+    while(completed != nProcessos) {
         idx = q.front();
         q.pop();
 
-        if(burst_remaining[idx] == p[idx].burst_time) {
-            p[idx].start_time = max(current_time,p[idx].arrival_time);
+        if (burst_cpy[idx] == p[idx].burst_time) {
+            p[idx].start_time = max(current_time, p[idx].arrival_time);
             total_idle_time += p[idx].start_time - current_time;
             current_time = p[idx].start_time;
         }
 
-        if(burst_remaining[idx]-tq > 0) {
-            burst_remaining[idx] -= tq;
-            current_time += tq;
-        }
-        else {
-            current_time += burst_remaining[idx];
-            burst_remaining[idx] = 0;
+        if (burst_cpy[idx] - tempoQuantum > 0) {
+            burst_cpy[idx] -= tempoQuantum;
+            current_time += tempoQuantum;
+            q.push(idx);
+        } else {
+            current_time += burst_cpy[idx];
+            burst_cpy[idx] = 0;
             completed++;
 
             p[idx].completion_time = current_time;
@@ -98,41 +99,20 @@ int main() {
             total_response_time += p[idx].response_time;
         }
 
-        for(int i = 1; i < n; i++) {
-            if(burst_remaining[i] > 0 && p[i].arrival_time <= current_time && mark[i] == 0) {
-                q.push(i);
-                mark[i] = 1;
-            }
-        }
-        if(burst_remaining[idx] > 0) {
-            q.push(idx);
-        }
-
-        if(q.empty()) {
-            for(int i = 1; i < n; i++) {
-                if(burst_remaining[i] > 0) {
-                    q.push(i);
-                    mark[i] = 1;
-                    break;
-                }
-            }
-        }
-
-
     }
 
-    avg_turnaround_time = (float) total_turnaround_time / n;
-    avg_waiting_time = (float) total_waiting_time / n;
-    avg_response_time = (float) total_response_time / n;
-    cpu_utilisation = ((p[n-1].completion_time - total_idle_time) / (float) p[n-1].completion_time)*100;
-    throughput = float(n) / (p[n-1].completion_time - p[0].arrival_time);
+    avg_turnaround_time = (float) total_turnaround_time / nProcessos;
+    avg_waiting_time = (float) total_waiting_time / nProcessos;
+    avg_response_time = (float) total_response_time / nProcessos;
+    cpu_utilisation = ((p[nProcessos-1].completion_time - total_idle_time) / (float) p[nProcessos-1].completion_time)*100;
+    throughput = float(nProcessos) / (p[nProcessos-1].completion_time - p[0].arrival_time);
 
-    sort(p,p+n,compare2);
+    sort(p,p+nProcessos,organizarPorPids);
 
     cout<<endl;
     cout<<"#P\t"<<"AT\t"<<"BT\t"<<"ST\t"<<"CT\t"<<"TAT\t"<<"WT\t"<<"RT\t"<<"\n"<<endl;
 
-    for(int i = 0; i < n; i++) {
+    for(int i = 0; i < nProcessos; i++) {
         cout<<p[i].pid<<"\t"<<p[i].arrival_time<<"\t"<<p[i].burst_time<<"\t"<<p[i].start_time<<"\t"<<p[i].completion_time<<"\t"<<p[i].turnaround_time<<"\t"<<p[i].waiting_time<<"\t"<<p[i].response_time<<"\t"<<"\n"<<endl;
     }
     cout<<"Average Turnaround Time = "<<avg_turnaround_time<<endl;
